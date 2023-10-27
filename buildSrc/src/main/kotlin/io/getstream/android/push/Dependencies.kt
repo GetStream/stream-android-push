634ff7a36cd6f8208c97ae2f84a87842455d75bf
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-10-27 16:23:53 +0200

    Upgrade Huawei dependencies

--- buildSrc/src/main/kotlin/io/getstream/android/push/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/getstream/android/push/Dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
     internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
     internal const val GRADLE_VERSIONS_PLUGIN = "0.49.0"
     internal const val HUAWEI_AGCP = "1.7.3.302"
-    internal const val HUAWEI_PUSH = "6.7.0.300"
+    internal const val HUAWEI_PUSH = "6.11.0.300"
     internal const val JUNIT4 = "4.13.2"
     internal const val JUNIT5 = "5.10.0"
     internal const val JSON = "20220924"
