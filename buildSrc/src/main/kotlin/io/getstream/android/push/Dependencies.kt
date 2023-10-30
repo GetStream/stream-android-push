ad01111c335eb2f6b8606b81a33a02450656fe5a
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-10-30 09:51:30 +0100

    Update Gradle Nexus Pbulish Plugin Dependency

--- buildSrc/src/main/kotlin/io/getstream/android/push/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/getstream/android/push/Dependencies.kt
@@ -18,7 +18,7 @@ object Versions {
     internal const val ESPRESSO = "3.5.1"
     internal const val FIREBASE_MESSAGING = "23.3.1"
     internal const val GITVERSIONER = "0.5.0"
-    internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
+    internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.3.0"
     internal const val GRADLE_VERSIONS_PLUGIN = "0.49.0"
     internal const val HUAWEI_AGCP = "1.7.3.302"
     internal const val HUAWEI_PUSH = "6.11.0.300"
